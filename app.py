ffd7d2a10d619b7f63a7b48d10c39bebb1a1ac6b
Alfred Chiesa <alfred@villaroad.com>
2011-08-02 15:48:31 -0400

    added a couple files for good segregation

--- app.py
+++ app.py
@@ -1,2 +1,19 @@
 from BeautifulSoup import BeautifulSoup as BS
 import urllib, urllib2
+
+
+class Scraper():
+    def __init__(self):
+        """
+        Initializes the scraper with all the proper attributes and information
+        that is needed to successuly scrape.
+        """
+        headers = {"User-Agent": USER_AGENT}
+        data = urllib.urlencode(values)
+        url = "%s?&Make=%s&Model=%s&LocationID=%s" % (PP_URL, MAKE, MODEL, LOCATION)
+        request = urllib2.Request(url, data, headers)
+        response = urllib2.urlopen(request)
+        page_response = response.read()
+        pool = BeautifulSoup(page_response)
+        
+        results = pool.findAll('div', attrs={'class':'inventory'})
