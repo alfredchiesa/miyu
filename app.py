157d862df27cf89883f269c17416c2081942776c
Alfred Chiesa <alfred@villaroad.com>
2011-08-02 16:12:31 -0400

    another include

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from BeautifulSoup import BeautifulSoup as BS
-import urllib, urllib2
+import urllib, urllib2, settings
 
 
 class Scraper():
@@ -8,8 +8,9 @@ class Scraper():
         Initializes the scraper with all the proper attributes and information
         that is needed to successuly scrape.
         """
-        headers = {"User-Agent": USER_AGENT}
-        data = urllib.urlencode(values)
+        values = {}
+        self.headers = {"User-Agent": USER_AGENT}
+        self.data = urllib.urlencode(values)
         url = "%s?&Make=%s&Model=%s&LocationID=%s" % (PP_URL, MAKE, MODEL, LOCATION)
         request = urllib2.Request(url, data, headers)
         response = urllib2.urlopen(request)
