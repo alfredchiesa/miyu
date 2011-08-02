16f5eba2875a5dcff6461a45726793164a2f6803
Alfred Chiesa <alfred@villaroad.com>
2011-08-02 16:12:41 -0400

    removed the scraper

--- app.py
+++ app.py
@@ -1,20 +1,2 @@
 from BeautifulSoup import BeautifulSoup as BS
 import urllib, urllib2, settings
-
-
-class Scraper():
-    def __init__(self):
-        """
-        Initializes the scraper with all the proper attributes and information
-        that is needed to successuly scrape.
-        """
-        values = {}
-        self.headers = {"User-Agent": USER_AGENT}
-        self.data = urllib.urlencode(values)
-        url = "%s?&Make=%s&Model=%s&LocationID=%s" % (PP_URL, MAKE, MODEL, LOCATION)
-        request = urllib2.Request(url, data, headers)
-        response = urllib2.urlopen(request)
-        page_response = response.read()
-        pool = BeautifulSoup(page_response)
-        
-        results = pool.findAll('div', attrs={'class':'inventory'})
