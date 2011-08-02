73187e3d85e5f37528ba227735234fde465c7c54
Alfred Chiesa <alfred@villaroad.com>
2011-08-02 16:12:10 -0400

    started a settings file for the edits and such

---
+++ settings.py
@@ -0,0 +1,25 @@
+#==============================config miyu here================================#
+
+# the list of letters to search manufacturers with
+MANU_CHINA_ALPHA = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', \
+                    'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', \
+                    'w', 'x', 'y', 'z']
+MANU_CRYSTAL_ALPHA = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', \
+                      'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', \
+                    'w', 'x', 'y', 'z']
+MANU_SILVER_ALPHA = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', \
+                     'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', \
+                     'w', 'x', 'y', 'z']
+
+
+# the list of manufacturers urls
+MANU_CHINA_URL = "http://replacements.com/china/manu/"
+MANU_CRYSTAL_URL = "http://replacements.com/crystal/manu/"
+MANU_SILVER_URL = "http://replacements.com/silver/manu/"
+
+#==============================================================================#
+#==============================================================================#
+#==============================================================================#
+#==========================do not edit this stuff==============================#
+USER_AGENT = 'Mozilla/5 (Macintosh; U; Intel Mac OS X 10_6_5; en-us) Gecko'
+#==============================================================================#
